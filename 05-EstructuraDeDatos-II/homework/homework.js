4bed7d6f78e60e7d7b53335200f960eaa133070c
CrisGab22 <cristhianverarivas22@gmail.com>
2022-09-20 15:39:05 -0500

    Algoritmos-01 terminado

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -1,19 +1,67 @@
 "use strict";
 
+const { concurrency } = require("@11ty/eleventy-cache-assets");
+const { lib } = require("markdown-it/lib/common/utils");
+
 /*
 Implementar la clase LinkedList, definiendo los siguientes métodos:
   - add: agrega un nuevo nodo al final de la lista;
   - remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular de una lista de un solo nodo y de una lista vacía);
-  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
+  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser un valor o un callback. En el primer caso,
+   buscamos un nodo cuyo valor coincida con lo buscado; en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
   Ejemplo: 
   search(3) busca un nodo cuyo valor sea 3;
   search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
 
-function LinkedList() {}
+function LinkedList() {
+  this.head = null
+}
 
-function Node(value) {}
+function Node(value) {
+  this.value = value
+  this.next = null
+}
+
+
+LinkedList.prototype.add = function(value){
+  let node = new Node(value);
+  let current = this.head
+  if(!current){ 
+    this.head = node;
+    return node;
+  }
+  while (current.next){
+    current = current.next
+  }
+  current.next = node
+  return node
+}
+LinkedList.prototype.remove = function(){
+  let current = this.head
+  if(current == null) return null
+  else if(current && !current.next){
+    let aux = current.value
+    current = null
+    return aux
+  }
+  while(current.next.next){
+    current = current.next
+  }
+  let aux = current.next.value
+  current.next = null
+  return aux
+}
+
+LinkedList.prototype.search = function(dato){
+  let current = this.head
+  while(current.value !== dato && current){
+    current = current.next
+  }
+  return current.value
+  }
+
 
 /*
 Implementar la clase HashTable.
--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  var  array = [1], i = 2
+    while(num!== 1){
+      if(num%i== 0){
+        array.push(i)
+        num = num/i
+      }
+      else{
+        i++
+      }
+    }
+  return array
 }
 
 function bubbleSort(array) {
@@ -14,7 +24,15 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let n= 1;n<array.length;n++){
+    for(let i= 0;i<array.length;i++)
+    if(array[i]> array[i+1]){
+      let aux= array[i]
+      array[i] = array[i+1]
+      array[i+1] = aux
+    }
+  }
+ return array
 }
 
 
@@ -23,7 +41,24 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+    
+let acum=[]
+acum.push(array[0])
 
+for(let i=1; i<array.length;i++){
+    
+    acum.unshift(array[i])
+
+      for(let n = 0;n<acum.length; n++){
+      
+        if(acum[n]>acum[n+1]){
+          let max = acum[n]
+          acum[n] = acum[n+1]
+          acum[n+1] = max
+      }
+  }
+}
+return acum
 }
 
 
@@ -32,7 +67,22 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+for(let n = 0; n< array.length;n++){
+  let min = array[n]
+  let location
+    for(let i =n+1;i<array.length;i++){
+      if(min>array[i]){
+        min = array[i]
+        location = i
+      }
+  }
+  if(location){
+    array[location] = array[n]
+    array[n] = min   
+  }
+    
+  }
+  return array
 }
 
 
